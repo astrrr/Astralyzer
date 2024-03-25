093a6129468a7036871679a15427518837db1029
Astr <invisibile.sek@gmail.com>
2024-03-26 00:26:15 +0700

    refactor: 💡 plot_volatility_histogram function
    - Replaced figtext with plt.text to directly add text annotations to the
    plot

--- astralyzer/astralyzer.py
+++ astralyzer/astralyzer.py
@@ -37,33 +37,26 @@ class Astralyzer:
         
         return vol_mean, vol_std, vol_1std_plus, vol_2std_plus, vol_3std_plus, vol_1std_minus, vol_2std_minus, vol_3std_minus
 
-    def visualize_volatility(self):
+    def visualize_volatility(self, width=8, height=6):
         vol_mean, vol_std, vol_1std_plus, vol_2std_plus, vol_3std_plus, vol_1std_minus, vol_2std_minus, vol_3std_minus = self.calculate_volatility_stats()
+    
+        plt.figure(figsize=(width, height))
     
         sns.histplot(data=self.data, bins=60, x="Volatility", kde=True)
         plt.title(f'{self.product_name} Volatility {self.data.iloc[0]["Date"]} - {self.data.iloc[-1]["Date"]}')
         
         plt.axvline(vol_mean, color='Green', linewidth=2, linestyle="--", label="Mean")
-        plt.figtext(0.3, 0.50, f"Mean: {vol_mean*100:.7f} %", fontsize=10)
-        
         plt.axvline(vol_1std_plus, color='Blue', linewidth=2, linestyle="--", label="1SD Upper")
-        plt.figtext(0.3, 0.60, f"1SD Upper: {vol_1std_plus*100:.7f} %", fontsize=10)
-        
         plt.axvline(vol_2std_plus, color='Coral', linewidth=2, linestyle="--", label="2SD Upper")
-        plt.figtext(0.3, 0.70, f"2SD Upper: {vol_2std_plus*100:.7f} %", fontsize=10)
-        
         plt.axvline(vol_3std_plus, color='Red', linewidth=2, linestyle="--", label="3SD Upper")
-        plt.figtext(0.3, 0.80, f"3SD Upper: {vol_3std_plus*100:.7f} %", fontsize=10)
-        
         plt.axvline(vol_1std_minus, color='Blue', linewidth=2, linestyle="--", label="1SD Lower")
-        plt.figtext(0.3, 0.40, f"1SD Lower: {vol_1std_minus*100:.7f} %", fontsize=10)
-        
         plt.axvline(vol_2std_minus, color='Coral', linewidth=2, linestyle="--", label="2SD Lower")
-        plt.figtext(0.3, 0.30, f"2SD Lower: {vol_2std_minus*100:.7f} %", fontsize=10)
-        
         plt.axvline(vol_3std_minus, color='Red', linewidth=2, linestyle="--", label="3SD Lower")
-        plt.figtext(0.3, 0.20, f"3SD Lower: {vol_3std_minus*100:.7f} %", fontsize=10)
         
+        text = f"Mean: {vol_mean*100:.7f} %\n\n1SD Upper: {vol_1std_plus*100:.7f} %\n2SD Upper: {vol_2std_plus*100:.7f} %\n3SD Upper: {vol_3std_plus*100:.7f} %\n\n1SD Lower: {vol_1std_minus*100:.7f} %\n2SD Lower: {vol_2std_minus*100:.7f} %\n3SD Lower: {vol_3std_minus*100:.7f} %"
+        plt.text(0.95, 0.05, text, fontsize=10, transform=plt.gca().transAxes,
+             verticalalignment='bottom', horizontalalignment='right', bbox=dict(facecolor='white', alpha=0.5))
+    
         plt.legend()
         plt.show()
 
