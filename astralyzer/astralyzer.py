509944bfe0786c56235f6f5d7840420d14254afa
Astr <invisibile.sek@gmail.com>
2024-03-28 01:08:09 +0700

    feat: 🎸 visualize_close

--- astralyzer/astralyzer.py
+++ astralyzer/astralyzer.py
@@ -25,38 +25,61 @@ class Astralyzer:
         vol = [((self.data.iloc[i]['High'] - self.data.iloc[i]['Low']) / self.data.iloc[i]['Open']) for i in range(len(self.data))]
         self.data['Volatility'] = vol
         
-    def calculate_volatility_stats(self):
-        vol_mean = np.mean(self.data["Volatility"])
-        vol_std = np.std(self.data["Volatility"])
-        vol_1std_plus = vol_mean + vol_std
-        vol_2std_plus = vol_mean + (vol_std * 2)
-        vol_3std_plus = vol_mean + (vol_std * 3)
-        vol_1std_minus = vol_mean - vol_std
-        vol_2std_minus = vol_mean - (vol_std * 2)
-        vol_3std_minus = vol_mean - (vol_std * 3)
+    def calculate_stats(self, column):
+        mean = np.mean(self.data[column])
+        std = np.std(self.data[column])
+        std_plus_1 = mean + std
+        std_plus_2 = mean + (std * 2)
+        std_plus_3 = mean + (std * 3)
+        std_minus_1 = mean - std
+        std_minus_2 = mean - (std * 2)
+        std_minus_3 = mean - (std * 3)
         
-        return vol_mean, vol_std, vol_1std_plus, vol_2std_plus, vol_3std_plus, vol_1std_minus, vol_2std_minus, vol_3std_minus
+        return mean, std, std_plus_1, std_plus_2, std_plus_3, std_minus_1, std_minus_2, std_minus_3
 
     def visualize_volatility(self, width=8, height=6):
-        vol_mean, vol_std, vol_1std_plus, vol_2std_plus, vol_3std_plus, vol_1std_minus, vol_2std_minus, vol_3std_minus = self.calculate_volatility_stats()
+        mean, std, std_plus_1, std_plus_2, std_plus_3, std_minus_1, std_minus_2, std_minus_3 = self.calculate_volatility_stats(column="Volatility")
     
         plt.figure(figsize=(width, height))
     
         sns.histplot(data=self.data, bins=60, x="Volatility", kde=True)
         plt.title(f'{self.product_name} Volatility {self.data.iloc[0]["Date"]} - {self.data.iloc[-1]["Date"]}')
         
-        plt.axvline(vol_mean, color='Green', linewidth=2, linestyle="--", label="Mean")
-        plt.axvline(vol_1std_plus, color='Blue', linewidth=2, linestyle="--", label="1SD Upper")
-        plt.axvline(vol_2std_plus, color='Coral', linewidth=2, linestyle="--", label="2SD Upper")
-        plt.axvline(vol_3std_plus, color='Red', linewidth=2, linestyle="--", label="3SD Upper")
-        plt.axvline(vol_1std_minus, color='Blue', linewidth=2, linestyle="--", label="1SD Lower")
-        plt.axvline(vol_2std_minus, color='Coral', linewidth=2, linestyle="--", label="2SD Lower")
-        plt.axvline(vol_3std_minus, color='Red', linewidth=2, linestyle="--", label="3SD Lower")
+        plt.axvline(mean, color='Green', linewidth=2, linestyle="--", label="Mean")
+        plt.axvline(std_plus_1, color='Blue', linewidth=2, linestyle="--", label="1SD Upper")
+        plt.axvline(std_plus_2, color='Coral', linewidth=2, linestyle="--", label="2SD Upper")
+        plt.axvline(std_plus_3, color='Red', linewidth=2, linestyle="--", label="3SD Upper")
+        plt.axvline(std_minus_1, color='Blue', linewidth=2, linestyle="--", label="1SD Lower")
+        plt.axvline(std_minus_2, color='Coral', linewidth=2, linestyle="--", label="2SD Lower")
+        plt.axvline(std_minus_3, color='Red', linewidth=2, linestyle="--", label="3SD Lower")
         
-        text = f"Mean: {vol_mean*100:.7f} %\n\n1SD Upper: {vol_1std_plus*100:.7f} %\n2SD Upper: {vol_2std_plus*100:.7f} %\n3SD Upper: {vol_3std_plus*100:.7f} %\n\n1SD Lower: {vol_1std_minus*100:.7f} %\n2SD Lower: {vol_2std_minus*100:.7f} %\n3SD Lower: {vol_3std_minus*100:.7f} %"
+        text = f"Mean: {mean*100:.7f} %\n\n1SD Upper: {std_plus_1*100:.7f} %\n2SD Upper: {std_plus_2*100:.7f} %\n3SD Upper: {std_plus_3*100:.7f} %\n\n1SD Lower: {std_minus_1*100:.7f} %\n2SD Lower: {std_minus_2*100:.7f} %\n3SD Lower: {std_minus_3*100:.7f} %"
         plt.text(0.95, 0.05, text, fontsize=10, transform=plt.gca().transAxes,
              verticalalignment='bottom', horizontalalignment='right', bbox=dict(facecolor='white', alpha=0.5))
     
         plt.legend()
         plt.show()
 
+    def visualize_close(self, width=8, height=6):
+            
+        mean, std, std_plus_1, std_plus_2, std_plus_3, std_minus_1, std_minus_2, std_minus_3 = self.calculate_volatility_stats(column="Close")
+    
+        plt.figure(figsize=(width, height))
+    
+        sns.histplot(data=self.data, bins=60, x="Close", kde=True)
+        plt.title(f'{self.product_name} Close {self.data.iloc[0]["Date"]} - {self.data.iloc[-1]["Date"]}')
+        
+        plt.axvline(mean, color='Green', linewidth=2, linestyle="--", label="Mean")
+        plt.axvline(std_plus_1, color='Blue', linewidth=2, linestyle="--", label="1SD Upper")
+        plt.axvline(std_plus_2, color='Coral', linewidth=2, linestyle="--", label="2SD Upper")
+        plt.axvline(std_plus_3, color='Red', linewidth=2, linestyle="--", label="3SD Upper")
+        plt.axvline(std_minus_1, color='Blue', linewidth=2, linestyle="--", label="1SD Lower")
+        plt.axvline(std_minus_2, color='Coral', linewidth=2, linestyle="--", label="2SD Lower")
+        plt.axvline(std_minus_3, color='Red', linewidth=2, linestyle="--", label="3SD Lower")
+        
+        text = f"Mean: {mean*100:.7f} %\n\n1SD Upper: {std_plus_1*100:.7f} %\n2SD Upper: {std_plus_2*100:.7f} %\n3SD Upper: {std_plus_3*100:.7f} %\n\n1SD Lower: {std_minus_1*100:.7f} %\n2SD Lower: {std_minus_2*100:.7f} %\n3SD Lower: {std_minus_3*100:.7f} %"
+        plt.text(0.95, 0.05, text, fontsize=10, transform=plt.gca().transAxes,
+             verticalalignment='bottom', horizontalalignment='right', bbox=dict(facecolor='white', alpha=0.5))
+    
+        plt.legend()
+        plt.show()
